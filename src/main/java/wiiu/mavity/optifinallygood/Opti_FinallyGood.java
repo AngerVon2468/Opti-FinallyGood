70bc307545534929f4cc7ef0797ebe2465d1975a
AngerVon2468 <thomaschad0909@gmail.com>
2024-06-22 15:36:58 +1000

    Initial Commit

---
+++ src/main/java/wiiu/mavity/optifinallygood/Opti_FinallyGood.java
@@ -0,0 +1,11 @@
+package wiiu.mavity.optifinallygood;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Opti_FinallyGood implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/wiiu/mavity/optifinallygood/Opti_FinallyGoodClient.java
@@ -0,0 +1,11 @@
+package wiiu.mavity.optifinallygood;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Opti_FinallyGoodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
