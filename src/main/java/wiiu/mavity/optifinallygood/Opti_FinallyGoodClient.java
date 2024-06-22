234eca35aa1fa588f5bbabc8d6fde8538be5b9a9
AngerVon2468 <thomaschad0909@gmail.com>
2024-06-22 19:45:06 +1000

    I forgot to ban optifine from loading

--- src/main/java/wiiu/mavity/optifinallygood/Opti_FinallyGoodClient.java
+++ src/main/java/wiiu/mavity/optifinallygood/Opti_FinallyGoodClient.java
@@ -1,11 +1,20 @@
 package wiiu.mavity.optifinallygood;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class Opti_FinallyGoodClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        if (FabricLoader.getInstance().isModLoaded("optifabric")) {
 
+            throw new RuntimeException("Are you actually stupid?");
+
+        } else if (FabricLoader.getInstance().isModLoaded("optifine")) {
+
+            throw new RuntimeException("Are you actually stupid?");
+
+        }
     }
 }
